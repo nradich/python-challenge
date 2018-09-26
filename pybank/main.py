0c54a1d6436a0e53c80ba25a785fe8e436f2abbe
Nicholas Radich <nicholasradich1@gmail.com>
2018-09-25 18:24:58 -0700

    Answer to Q3git add pybank

--- pybank/main.py
+++ pybank/main.py
@@ -5,6 +5,7 @@ csvpath= os.path.join('budgetdata.csv',)
 
 month =[]
 total = []
+difference = []
 
 with open ('budgetdata.csv' , newline= '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -19,10 +20,31 @@ with open ('budgetdata.csv' , newline= '') as csvfile:
     def number_of_months():
         """Generates the number of rows in the CSV file"""
         return(len(month))
-      
+
+
+  
+
+for x,y in zip(total,(total[1:])):
+        difference.append(y-x)
+print(difference)
+
+def avgmonth ():
+    return sum(difference)/len(difference)
+
+print(avgmonth())
+
+    #def avgmonth ():
+       # """Calculating the average change per month"""
+        #for i in total:
+           # avg = sum(i,i+1)
+           # print(avg)
+    #avgmonth()
          #def add_month(x):#return sum(x
     #  #x=month
-print(number_of_months())
-"""86"""
-print(sum(total))
-"""Second column added up"
+#print(number_of_months())
+#"""86"""
+#print(sum(total))
+#"""Second column added up"""
+#x= number_of_months()
+    #y= sum(total)
+    #print(y/x)
